f7037d2b9c18b7a5738e5642595070ffbccbcb42
Manav1888 <manav@simplifys.com>
2024-07-15 19:35:17 +0530

    dd

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,34 +1,66 @@
 import React from "react";
 import { motion } from "framer-motion";
 import css from "./Portfolio.module.scss";
-import { fadeIn, staggerChildren, textVariant, textVariant2 } from "../../utils/motion";
+import {
+  fadeIn,
+  staggerChildren,
+  textVariant,
+  textVariant2,
+} from "../../utils/motion";
 const Portfolio = () => {
   return (
     <motion.section
-    variants={staggerChildren}
-    initial="hidden"
-    whileInView="show"
-    viewport={{ once: false, amount: 0.25 }}
-    className={`paddings ${css.wrapper}`}>
-
+      variants={staggerChildren}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`paddings ${css.wrapper}`}
+    >
       <a className="anchor" id="portfolio"></a>
 
       <div className={`innerWidth flexCenter ${css.container}`}>
-
-
-        <motion.div variants={textVariant(.4)} className={`flexCenter ${css.heading}`}>
+        <motion.div
+          variants={textVariant(0.4)}
+          className={`flexCenter ${css.heading}`}
+        >
           <div>
             <span className="primaryText">My Latest Works</span>
-            <p style={{marginTop: "10px"}}>Perfect solution for digital experience</p>
+            <p style={{ marginTop: "10px" }}>
+              Perfect solution for digital experience
+            </p>
           </div>
           <span className="secondaryText">Explore More Works</span>
         </motion.div>
 
-
         <div className={`flexCenter ${css.showCase}`}>
-            <motion.img variants={fadeIn("up", "tween", .5, .6)} src="./showCase1.png" alt="project" />
-            <motion.img variants={fadeIn("up", "tween", .7, .6)} src="./showCase2.png" alt="project" />
-            <motion.img variants={fadeIn("up", "tween", .9, .6)} src="./showCase3.png" alt="project" />
+          <motion.img
+            variants={fadeIn("up", "tween", 0.5, 0.6)}
+            onClick={() =>
+              window.open(
+                "https://fajilashaikh.blogspot.com/2021/03/khela-hobe-conundrum-of-political.html",
+                "_blank"
+              )
+            }
+            src="./showCase1.png"
+            alt="project"
+          />
+          <motion.img
+            variants={fadeIn("up", "tween", 0.7, 0.6)}
+            src="./showCase2.png"
+            alt="project"
+            style={{ cursor: "pointer" }}
+            onClick={() =>
+              window.open(
+                "http://snufunblog.blogspot.com/2021/06/on-street-food-kolkata-kathi-rolls-by.html",
+                "_blank"
+              )
+            }
+          />
+          <motion.img
+            variants={fadeIn("up", "tween", 0.9, 0.6)}
+            src="./showCase3.png"
+            alt="project"
+          />
         </div>
       </div>
     </motion.section>
